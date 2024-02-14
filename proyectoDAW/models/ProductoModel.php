3c146f93557c4d130807049149aa492534011ec1
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-02-14 14:05:00 -0600

    Update ProductoModel.php

--- proyectoDAW/models/ProductoModel.php
+++ proyectoDAW/models/ProductoModel.php
@@ -7,12 +7,21 @@ class ProductoModel
   {
     $this->enlace = new MySqlConnect();
   }
-
+/**
+     * Listar productos
+     * @param 
+     * @return $vResultado - Lista de produtos
+     */
   public function all()
   {
-    try 
-    {
-    $vSQL = "SELECT * FROM producto;";
+    try {
+      $vSQL = "SELECT * FROM producto;";
+      $vResultado = $this->enlace->ExecuteSQL($vSQL);
+
+      return $vResultado;
+
+    } catch (Exception $e) {
+      die("" . $e->getMessage());
     }
   }
 }
