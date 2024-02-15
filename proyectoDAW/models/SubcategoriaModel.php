1c8c6bc1cc7ead33ed9b1b866d3b72d499c7842a
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-02-15 11:52:55 -0600

    Update SubcategoriaModel.php

--- proyectoDAW/models/SubcategoriaModel.php
+++ proyectoDAW/models/SubcategoriaModel.php
@@ -30,6 +30,7 @@ class SubcategoriaModel
   public function get($id)
   {
     try {
+      $categoriaM = new CategoriaModel();
       $vSql = "SELECT * from subcategoria where Idsubcategoria = $id";
 
       //Ejecutar la consulta sql
@@ -37,36 +38,15 @@ class SubcategoriaModel
       if (!empty($vResultado)) {
         //Obtener objeto
         $vResultado = $vResultado[0];
+
+        $categoria = $categoriaM->get($vResultado->IdCategoria);
+
+        $vResultado->IdCategoria = $categoria;
       }
       return $vResultado;
     } catch (Exception $e) {
       die($e->getMessage());
     }
+
   }
-  public function get($id)
-    {
-        //Instancia del modelo
-        $subcategoria = new SubcategoriaModel();
-        //Acción del modelo a ejecutar
-        $response = $subcategoria->get($id);
-        //Verificar respuesta
-        if (isset($response) && !empty($response)) {
-            //Armar el JSON respuesta satisfactoria
-            $json = array(
-                'status' => 200,
-                'results' => $response
-            );
-        } else {
-            //JSON respuesta negativa
-            $json = array(
-                'status' => 400,
-                'results' => "No existe el recurso solicitado"
-            );
-        }
-        //Escribir respuesta JSON con código de estado HTTP
-        echo json_encode(
-            $json,
-            http_response_code($json["status"])
-        );
-    }
 }
