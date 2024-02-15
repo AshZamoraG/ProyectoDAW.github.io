cf61cf0b0a0d8914556e545f456d51dbcb9d6f73
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-02-15 10:47:59 -0600

    Update index.php

--- proyectoDAW/index.php
+++ proyectoDAW/index.php
@@ -14,10 +14,14 @@ require_once "models/MySqlConnect.php";
 /***--- Agregar todos los modelos*/
 require_once "models/UsuarioModel.php";
 require_once "models/ProductoModel.php";
+require_once "models/Categoria.php";
+require_once "models/Subcategoria.php";
 
 /***--- Agregar todos los controladores*/
 require_once "controllers/UsuarioController.php";
 require_once "controllers/ProductoController.php";
+require_once "controllers/CategoriaController.php";
+require_once "controllers/SubcategoriaController.php";
 //Enrutador
 //RoutesController.php
 require_once "controllers/RoutesController.php";
