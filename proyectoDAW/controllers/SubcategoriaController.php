a001d0d3371104e16d670540d8fa3c861ecb97a7
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-02-15 11:53:20 -0600

    Update SubcategoriaController.php

--- proyectoDAW/controllers/SubcategoriaController.php
+++ proyectoDAW/controllers/SubcategoriaController.php
@@ -24,4 +24,30 @@ class subcategoria
       http_response_code($json["status"])
     );
   }
+   public function get($id)
+    {
+        //Instancia del modelo
+        $subcategoria = new SubcategoriaModel();
+        //Acción del modelo a ejecutar
+        $response = $subcategoria->get($id);
+        //Verificar respuesta
+        if (isset($response) && !empty($response)) {
+            //Armar el JSON respuesta satisfactoria
+            $json = array(
+                'status' => 200,
+                'results' => $response
+            );
+        } else {
+            //JSON respuesta negativa
+            $json = array(
+                'status' => 400,
+                'results' => "No existe el recurso solicitado"
+            );
+        }
+        //Escribir respuesta JSON con código de estado HTTP
+        echo json_encode(
+            $json,
+            http_response_code($json["status"])
+        );
+    }
 }
