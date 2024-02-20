1d92f1840efd9e21b434b13afce243f2362f08e5
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-02-20 15:30:58 -0600

    Add files via upload

---
+++ proyectoDAW/ProyectoDAW/src/themes/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+export const appTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#2d2152',
+    },
+    secondary: {
+      main: '#00fdfd',
+    },
+  },
+});
+
