4564716e1b444188be46281e6fca5b29afcd0fa3
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-02-27 17:17:58 -0600

    Add files via upload

---
+++ proyectoDAW/controllers/InventarioController.php
@@ -0,0 +1,52 @@
+<?php
+
+class Inventario
+{
+  public function index()
+  {
+    $inventarioM = new InventarioModel;
+    $response = $inventarioM->all();
+    if (isset($response) && !empty($response)) {
+      $json = array(
+        'status' => 200,
+        'results' => $response
+      );
+    } else {
+      $json = array(
+        'status' => 400,
+        'results' => "No hay registros"
+     );
+    }
+    echo json_encode($json,
+    http_response_code($json["status"]));
+  }
+
+  /* Llamar producto (nombre y en que bodega esta)-inventario 
+  public function get($id)
+  {
+      //Instancia del modelo
+      $inventarioM = new InventarioModel();
+      //Acción del modelo a ejecutar
+      $response = $inventarioM->get($id);
+      //Verificar respuesta
+      if (isset($response) && !empty($response)) {
+          //Armar el JSON respuesta satisfactoria
+          $json = array(
+              'status' => 200,
+              'results' => $response
+          );
+      } else {
+          //JSON respuesta negativa
+          $json = array(
+              'status' => 400,
+              'results' => "No existe el recurso solicitado"
+          );
+      }
+      //Escribir respuesta JSON con código de estado HTTP
+      echo json_encode(
+          $json,
+          http_response_code($json["status"])
+      );
+  }*/
+
+}
---
+++ proyectoDAW/controllers/ProductoController.php
@@ -0,0 +1,50 @@
+<?php
+
+class producto
+{
+  public function index()
+  {
+    $productoM = new ProductoModel;
+    $response = $productoM->all();
+    if (isset($response) && !empty($response)) {
+      $json = array(
+        'status' => 200,
+        'results' => $response
+      );
+    } else {
+      $json = array(
+        'status' => 400,
+        'results' => "No hay registros"
+      );
+    }
+    echo json_encode($json,
+    http_response_code($json["status"]));
+  }
+ 
+  public function get($id)
+    {
+        //Instancia del modelo
+        $prodcutoM = new ProductoModel();
+        //Acción del modelo a ejecutar
+        $response = $prodcutoM->get($id);
+        //Verificar respuesta
+        if (isset($response) && !empty($response)) {
+            //Armar el JSON respuesta satisfactoria
+            $json = array(
+                'status' => 200,
+                'results' => $response
+            );
+        } else {
+            //JSON respuesta negativa
+            $json = array(
+                'status' => 400,
+                'results' => "No existe el recurso solicitado"
+            );
+        }
+        //Escribir respuesta JSON con código de estado HTTP
+        echo json_encode(
+            $json,
+            http_response_code($json["status"])
+        );
+    }
+}
