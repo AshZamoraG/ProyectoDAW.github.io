756e641435d859912938b6ea7502930bf344648c
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-03-01 21:02:34 -0600

    Update InventarioController.php

--- proyectoDAW/controllers/InventarioController.php
+++ proyectoDAW/controllers/InventarioController.php
@@ -21,7 +21,7 @@ class Inventario
     http_response_code($json["status"]));
   }
 
-  /* Llamar producto (nombre y en que bodega esta)-inventario 
+
   public function get($id)
   {
       //Instancia del modelo
@@ -47,6 +47,32 @@ class Inventario
           $json,
           http_response_code($json["status"])
       );
-  }*/
+  }
+  public function getByUsuarioId($id)
+  {
+      //Instancia del modelo
+      $inventarioM = new InventarioModel();
+      //Acción del modelo a ejecutar
+      $response = $inventarioM->getByUsuarioId($id);
+      //Verificar respuesta
+      if (isset($response) && !empty($response)) {
+          //Armar el JSON respuesta satisfactoria
+          $json = array(
+              'status' => 200,
+              'results' => $response
+          );
+      } else {
+          //JSON respuesta negativa
+          $json = array(
+              'status' => 400,
+              'results' => "No existe el recurso solicitado"
+          );
+      }
+      //Escribir respuesta JSON con código de estado HTTP
+      echo json_encode(
+          $json,
+          http_response_code($json["status"])
+      );
+  }
 
 }
