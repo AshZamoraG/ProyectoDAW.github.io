6928407b0856a82010d4c3b03e627b1b09df6fba
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-03-03 19:20:15 -0600

    Add files via upload

---
+++ proyectoDAW/ProyectoDAW/src/services/OrdenCompraService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';//fetch
+const BASE_URL = import.meta.env.VITE_BASE_URL + 'ordencompra';
+class OrdenCompraService {
+  //Definición para Llamar al API y obtener el listado de películas
+  //localhost:81/proyectoDAW/producto
+  getOrdenCompra() {
+    return axios.get(BASE_URL);
+  }
+  //Obtener una prodcuto
+  //localhost:81/proyectoDAW/producto/2
+  getOrdenCompraById(Id){
+    return axios.get(BASE_URL+"/getOrdenCompraById/"+Id)
+  }
+}
+export default new OrdenCompraService();
