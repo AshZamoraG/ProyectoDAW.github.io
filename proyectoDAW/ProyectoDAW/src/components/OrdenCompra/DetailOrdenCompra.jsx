d67374fa78bec46b91e64bebedd1a6c5085740df
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-03-04 16:11:26 -0600

    Update DetailOrdenCompra.jsx

--- proyectoDAW/ProyectoDAW/src/components/OrdenCompra/DetailOrdenCompra.jsx
+++ proyectoDAW/ProyectoDAW/src/components/OrdenCompra/DetailOrdenCompra.jsx
@@ -1,108 +1,92 @@
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Grid} from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import OrdenCompraService from '../../services/OrdenCompraService';
 
 export function DetailOrdenCompra() {
-  const routeParams= useParams();
-  console.log(routeParams)
+  const routeParams = useParams();
+  console.log(routeParams);
 
-   //Resultado de consumo del API, respuesta
- const[data,setData]=useState(null);
- //Error del API
- const[error,setError]=useState('');
- //Booleano para establecer sí se ha recibido respuesta
- const[loaded,setLoaded]=useState(false);
-   useEffect(()=>{
+  //Resultado de consumo del API, respuesta
+  const [data, setData] = useState(null);
+  //Error del API
+  const [error, setError] = useState('');
+  //Booleano para establecer sí se ha recibido respuesta
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
     //Llamar al API y obtener una pelicula
     OrdenCompraService.OrdenCompraFactura(routeParams.Id)
-    .then( response=>{
-      setData(response.data.results)
-      console.log(response.data)
-      setError(response.error)
-      setLoaded(true)
-    }
-    ).catch( error=>{
-      console.log(error)
-      setError(error)
-      throw new Error("Respuesta no válida del servidor")
-    }      
-    )
-  },[routeParams.Id]) 
+      .then((response) => {
+        setData(response.data.results);
+        console.log(response.data);
+        setError(response.error);
+        setLoaded(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+        throw new Error('Respuesta no válida del servidor');
+      });
+  }, [routeParams.Id]);
 
-  if(!loaded) return <p>Cargando...</p>
-  if(error) return <p>Error: {error.message}</p>
+  if (!loaded) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
-    <Container component='main' sx={{ mt: 8, mb: 2 }}>
-      <Typography variant='h4' component='h1' gutterBottom>
+    <Container component="main" sx={{ mt: 8, mb: 2 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
         Factura
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Orden de Compra: N°{data.OrdenCompraId}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Fecha de Generación: {data.FechaGeneracion}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Fecha de Recepción: {data.FechaRecepcion}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Usuario: {data.CodigoUsuario} - {data.NombreUsuario}
-            </Box>
-          </Typography>
+      <Paper
+        elevation={3}
+        sx={{ padding: 2, backgroundColor: 'black', color: 'white' }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="subtitle1">
+              <strong>Orden de Compra:</strong> N°{data.OrdenCompraId}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Fecha de Generación:</strong> {data.FechaGeneracion}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Fecha de Recepción:</strong> {data.FechaRecepcion}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Usuario:</strong> {data.CodigoUsuario} -{' '}
+              {data.NombreUsuario}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="subtitle1">
+              <strong>Producto:</strong> {data.CodigoProducto} -{' '}
+              {data.NombreProducto}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>SKU:</strong> {data.CodigoSKU}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Descripción:</strong> {data.DescripcionProducto}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Proveedor:</strong> {data.CodigoProveedor} -{' '}
+              {data.NombreProveedor}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1">
+              <strong>Cantidad:</strong> {data.Cantidad}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Precio Unitario:</strong> {data.PrecioUnidad}
+            </Typography>
+            <Typography variant="subtitle1">
+              <strong>Total:</strong> {data.PrecioUnidad * data.Cantidad}
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Producto: {data.CodigoProducto} - {data.NombreProducto}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              SKU: {data.CodigoSKU}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Descripción: {data.DescripcionProducto}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Proveedor: {data.CodigoProveedor} - {data.NombreProveedor}
-            </Box>
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Cantidad: {data.Cantidad}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Precio Unitario: {data.PrecioUnidad}
-            </Box>
-          </Typography>
-          <Typography component='span' variant='subtitle1' display='block'>
-            <Box fontWeight='bold' display='inline'>
-              Total: {data.PrecioUnidad * data.Cantidad}  
-            </Box>
-          </Typography>
-        </Grid>
-      </Grid>
+      </Paper>
     </Container>
   );
 }
