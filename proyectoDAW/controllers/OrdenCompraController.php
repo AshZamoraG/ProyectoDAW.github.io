663edbe637686f975889c3203f6665be2b29c03f
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-03-04 15:53:33 -0600

    Update OrdenCompraController.php

--- proyectoDAW/controllers/OrdenCompraController.php
+++ proyectoDAW/controllers/OrdenCompraController.php
@@ -74,4 +74,32 @@ class ordencompra
           http_response_code($json["status"])
       );
   }
+
+  
+  public function OrdenCompraFactura($id)
+  {
+      //Instancia del modelo
+      $ordencompraM = new OrdenCompraModel();
+      //Acción del modelo a ejecutar
+      $response = $ordencompraM->OrdenCompraFactura($id);
+      //Verificar respuesta
+      if (isset($response) && !empty($response)) {
+          //Armar el JSON respuesta satisfactoria
+          $json = array(
+              'status' => 200,
+              'results' => $response
+          );
+      } else {
+          //JSON respuesta negativa
+          $json = array(
+              'status' => 400,
+              'results' => "No existe el recurso solicitado"
+          );
+      }
+      //Escribir respuesta JSON con código de estado HTTP
+      echo json_encode(
+          $json,
+          http_response_code($json["status"])
+      );
+  }
 }
