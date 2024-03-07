65994d95cbe2c2d2004fdcfdc73febbb098c274d
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-03-07 16:03:11 -0600

    Add files via upload

---
+++ proyectoDAW/ProyectoDAW/src/main.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Home } from './components/Home/Home.jsx'
+import { ListProducto } from './components/Producto/ListProducto.jsx'
+import { PageNotFound } from './components/Home/PageNotFound.jsx'
+import { DetailProducto } from './components/Producto/DetailProducto.jsx'
+import TableProducto from './components/Producto/TableProducto.jsx'
+import { ListOrdenCompra } from './components/OrdenCompra/ListOrdenCompra.jsx'
+//import { DetailOrdenCompra } from './components/OrdenCompra/DetailOrdenCompra.jsx'
+import { ListInventario } from './components/Inventario/ListInventario.jsx'
+import { DetailOrdenCompra } from './components/OrdenCompra/DetailOrdenCompra.jsx'
+import { DetailInventario } from './components/Inventario/DetailInventario.jsx'
+
+
+
+
+const rutas=createBrowserRouter([
+  {
+    element: <App />,
+    children:[
+      {
+        path:'/',
+        element: <Home />
+      },
+      {
+        path:'*',
+        element: <PageNotFound />
+      },
+      {
+        path:'/producto',
+        element: <ListProducto />
+      },
+      {
+        path:'/ordencompra',
+        element: <ListOrdenCompra />
+      },
+      {
+        path:'/inventario',
+        element: <ListInventario />
+      },
+      {
+        path:'/DetailInventario/:id',
+        element: <DetailInventario />
+      },
+      {
+        path:'producto-table',
+        element: <TableProducto />
+      },
+      {
+        path:'/producto/:Id',
+        element: <DetailProducto />
+      },
+      {
+        path:'/ordencompra/:Id',
+        element: <DetailOrdenCompra />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={rutas} />
+  </React.StrictMode>,
+)
---
+++ proyectoDAW/ProyectoDAW/src/themes/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+export const appTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#2d2152',
+    },
+    secondary: {
+      main: '#00fdfd',
+    },
+    text: {
+      primary: '#020202',
+      
+    },
+  },
+});
+
