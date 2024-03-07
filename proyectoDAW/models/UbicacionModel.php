0cff49ab252acdfacf2cb28dc0c08d5e614c552b
Apolo1170 <116931792+Apolo1170@users.noreply.github.com>
2024-03-07 15:59:59 -0600

    Add files via upload

---
+++ proyectoDAW/models/UbicacionModel.php
@@ -0,0 +1,48 @@
+<?php
+class UbicacionModel
+{
+
+    //Conectarse a la BD
+    public $enlace;
+
+    public function __construct()
+    {
+        $this->enlace = new MySqlConnect();
+    }
+
+    /**
+     * Listar Usuarios
+     * @param 
+     * @return $vResultado - Lista de objetos
+     */
+    public function all()
+  {
+    try {
+      $vSQL = "SELECT * FROM ubicacion;";
+      $vResultado = $this->enlace->ExecuteSQL($vSQL);
+
+      return $vResultado;
+
+    } catch (Exception $e) {
+      die("" . $e->getMessage());
+    }
+  }
+  public function get($id)
+  {
+    try {
+      $vSql = "SELECT * from ubicacion where Id = $id";
+
+      //Ejecutar la consulta sql
+      $vResultado = $this->enlace->executeSQL($vSql);
+      if (!empty($vResultado)) {
+        //Obtener objeto
+        $vResultado = $vResultado[0];
+      }
+      return $vResultado;
+    } catch (Exception $e) {
+      die($e->getMessage());
+    }
+
+  }
+
+}
