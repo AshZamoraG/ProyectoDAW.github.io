926d33b6d6dd9dafea3043430d67d9cd7dba088c
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-03-07 16:27:43 -0600

    Update ListInventario.jsx

--- proyectoDAW/ProyectoDAW/src/components/Inventario/ListInventario.jsx
+++ proyectoDAW/ProyectoDAW/src/components/Inventario/ListInventario.jsx
@@ -1,30 +1,21 @@
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import { Link } from 'react-router-dom';
-import { Info } from '@mui/icons-material';
-import { useEffect, useState } from 'react';
-import InventoryIcon from '@mui/icons-material/Inventory';
+import React, { useEffect, useState } from 'react';
+import { Grid, Card, CardHeader, CardContent, Typography, CardActions, IconButton, Button } from '@mui/material';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import TaskIcon from '@mui/icons-material/Task';
+import { Link } from 'react-router-dom';
 import InventarioService from '../../services/InventarioService';
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 export function ListInventario() {
-  
-  //Resultado de consumo del API, respuesta
-  const [data, setData] = useState(null);
-  //Error del API
+  const [data, setData] = useState([]);
   const [error, setError] = useState('');
-  //Booleano para establecer sí se ha recibido respuesta
   const [loaded, setLoaded] = useState(false);
-  const IdUsuario= 3;
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const idUsuario = 3;
 
   useEffect(() => {
-
-    InventarioService.getByIdUsuario(IdUsuario)
+    InventarioService.getByIdUsuario(idUsuario)
       .then((response) => {
         if (response.data) {
           setData(response.data.results);
@@ -38,53 +29,80 @@ export function ListInventario() {
       });
   }, []);
 
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
   if (!loaded) return <p>Cargando...</p>;
   if (error) return <p>Error: {error}</p>;
+
   return (
-    <Grid container style={{ padding: 2 }} spacing={3}>
-      {data && data.map((item)=>(  
-        <Grid item xs={4} key={item.id} >
-        <Card>
-          <CardHeader
-            sx={{
-              p: 0,
-              backgroundColor: (theme) => theme.palette.secondary.main,
-              color: (theme) => theme.palette.common.white,
-            }}
-            style={{ textAlign: 'center' }}
-            title= {item.Id} 
-            subheader={item.UsuarioRegistro}
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              <CalendarMonthIcon /> Fecha de Registro: {item.FechaRegistro}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              <CalendarMonthIcon /> Fecha de Actualizacion: {item.FechaActualizacion}  
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              <InventoryIcon /> Cantidad Disponible: {item.CantidadDisponible}  
-            </Typography>
-          </CardContent>
-          <CardActions
-            disableSpacing
-            sx={{
-              backgroundColor: (theme) => theme.palette.action.focus,
-              color: (theme) => theme.palette.common.white,
-            }}
-          >
-            <IconButton
-              component={Link}
-              to={`/DetailInventario/${item.id}`}
-              aria-label="Detalle"
-              sx={{ ml: 'auto' }}
-            >
-              <Info />
-            </IconButton>
-          </CardActions>
-        </Card>
+    <>
+      <Grid container style={{ padding: 2 }} spacing={3} sx={{
+        backgroundColor: '#f5f5f5',
+
+      }}>
+        {currentItems.map((item) => (
+          <Grid item xs={6} key={item.Id}>
+            <Card sx={{
+              backgroundColor: '#f5f5f5',
+              transition: 'transform 0.3s ease-in-out, background-color 0.3s ease-in-out',
+              '&:hover': {
+                transform: 'scale(1.05)',
+
+              }
+            }}>
+              <CardHeader
+                sx={{
+                  p: 0,
+                  backgroundColor: (theme) => theme.palette.secondary.main,
+                  color: (theme) => theme.palette.common.white,
+                }}
+                style={{ textAlign: 'center' }}
+                title={ item.NombreBodega + " N° inventario "+ item.NombreProducto}
+              />
+              <CardContent >
+                <Typography variant="body2" color="text.secondary">
+                  <CalendarMonthIcon /> Fecha en que se Genero: {item.FechaRegistro}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <CalendarMonthIcon /> Fecha en la que se recibio: {item.FechaActualizacion}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  <InventoryIcon /> Cantidad Disponible : {item.CantidadDisponible}
+                </Typography>
+              </CardContent>
+              <CardActions
+                disableSpacing
+                sx={{
+                  backgroundColor: (theme) => theme.palette.action.focus,
+                  color: (theme) => theme.palette.common.white,
+                }}
+              >
+                <IconButton
+                  component={Link}
+                  to={`/DetailInventario/${item.id}`}
+                  aria-label="Detalle"
+                  sx={{ ml: 'auto' }}
+                >
+                  <TaskIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-     ))}
-    </Grid>
+      <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button key={index} onClick={() => paginate(index + 1)} style={{ margin: '0 5px' }}>
+            {index + 1}
+          </Button>
+        ))}
+      </div>
+    </>
   );
 }
