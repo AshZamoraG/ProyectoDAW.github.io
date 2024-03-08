897f895185fd4506b2fabacf8fe08c69572debf4
AshZamoraG <145407177+AshZamoraG@users.noreply.github.com>
2024-03-07 20:10:59 -0600

    Add files via upload

---
+++ proyectoDAW/models/CategoriaModel.php
@@ -0,0 +1,48 @@
+<?php
+class CategoriaModel
+{
+  //Conectarse a la BD
+  public $enlace;
+
+  public function __construct()
+  {
+    $this->enlace = new MySqlConnect();
+  }
+  /**
+   * Listar categorias
+   * @param 
+   * @return $vResultado - Lista de categorias
+   */
+  public function all()
+  {
+    try {
+      //Consulta SQL
+      $vSQL = "SELECT * FROM categoria;";
+      //Ejecutar la consulta
+      $vResultado = $this->enlace->ExecuteSQL($vSQL);
+      //Retornar la respuesta
+
+      return $vResultado;
+    } catch (Exception $e) {
+      die("" . $e->getMessage());
+    }
+  }
+    public function get($id)
+  {
+    try {
+      $vSql = "SELECT * from categoria where Id = $id";
+      
+      //Ejecutar la consulta sql
+      $vResultado = $this->enlace->executeSQL($vSql);
+      if (!empty($vResultado)) {
+        //Obtener objeto
+        $vResultado = $vResultado[0];
+
+      }
+      return $vResultado;
+    } catch (Exception $e) {
+      die($e->getMessage());
+    }
+
+  }
+}
